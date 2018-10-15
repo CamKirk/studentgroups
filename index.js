b1db0eb951517d463112286fee86daebfb31a20e
camkirk <camden.kirkland@gmail.com>
2018-10-14 21:15:36 -0500

    removal of error handler, restructure route

--- index.js
+++ index.js
@@ -9,29 +9,39 @@ const app = express();
 
 const db = mongoose.connect(process.env.MONGODB_URI);
 app.use(express.json());
-
+function errorHandler (err, req, res, next) {
+    res.status(500)
+    res.send('error', { error: err })
+  }
+app.use(errorHandler);
 app.get('/',(req, res)=>{
     res.header('Access-Control-Allow-Origin','*');
     res.json({data:"hello world"});
 });
 
-app.get('/api/:name/:week', (req, res, next)  => {
-    let name = req.params.name;
+app.get('/api/:name/:week', (req, res,next)  => {
+    let name = req.params.name.toLowerCase();
     let week = parseInt(req.params.week);
+
     if (!week||isNaN(week)){
-        res.send(200,new Error("you must send a week"));
+        res.status(400).json({err:("you must send a week number")});
+    }else {
+        model.Group.find({members:name, week:week}, (err, docs) => {
+            console.log(req.headers);
+            
+            if (err) res.send(err);
+            if (docs === null) res.json({err:"name not found"} );
+            else{
+                console.log(docs);
+                res.set('Access-Control-Allow-Origin', '*');
+                res.json(docs);
+            }
+        });
     }
+
     console.log(name, week);
     
-    model.Group.find({members:name, week:week}, (err, docs) => {
-        if (err) res.send(err);
-        if (docs === null) res.json({err:"name not found"} );
-        else{
-            console.log(docs);
-            res.header('Access-Control-Allow-Origin','*');
-            res.json(docs);
-        }
-    });
+    
 });
 
 app.listen(process.env.PORT || 4000, () => {
